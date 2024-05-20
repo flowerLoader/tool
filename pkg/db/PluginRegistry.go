2dd169c7d28b9a368ddeac0cf19f8f7e7562e6ca
Damon Blais <damon@rethunk.tech>
2024-05-19 23:40:49 -0700

    refactor(db): Extract PluginRegistry into a separate file

---
+++ pkg/db/PluginRegistry.go
@@ -0,0 +1,73 @@
+package db
+
+import "github.com/flowerLoader/tool/pkg/db/types"
+
+type PluginRegistry struct {
+	db *DB
+}
+
+func (p *PluginRegistry) CacheGet(id string) (*types.PluginCacheRecord, error) {
+	prepared := `SELECT
+		id, guid, version, name, author, tags, summary
+	FROM plugin_cache WHERE id = ?`
+
+	stmt, _, err := p.db.conn.Prepare(prepared)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	stmt.BindText(1, id)
+
+	if hasRow := stmt.Step(); !hasRow {
+		return nil, err
+	}
+
+	return &types.PluginCacheRecord{
+		ID:      stmt.ColumnText(0),
+		GUID:    stmt.ColumnText(1),
+		Version: stmt.ColumnText(2),
+		Name:    stmt.ColumnText(3),
+		Author:  stmt.ColumnText(4),
+		Tags:    stmt.ColumnText(5),
+		Summary: stmt.ColumnText(6),
+	}, nil
+}
+
+func (p *PluginRegistry) CacheUpdate(plugin types.PluginCacheRecord) error {
+	prepared := `INSERT INTO plugin_cache (
+		id, guid, version, name, author, tags, summary
+	) VALUES (
+		?, ?, ?, ?, ?, ?, ?
+	)`
+
+	stmt, _, err := p.db.conn.Prepare(prepared)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	if err := stmt.BindText(1, plugin.ID); err != nil {
+		return err
+	}
+	if err := stmt.BindText(2, plugin.GUID); err != nil {
+		return err
+	}
+	if err := stmt.BindText(3, plugin.Version); err != nil {
+		return err
+	}
+	if err := stmt.BindText(4, plugin.Name); err != nil {
+		return err
+	}
+	if err := stmt.BindText(5, plugin.Author); err != nil {
+		return err
+	}
+	if err := stmt.BindText(6, plugin.Tags); err != nil {
+		return err
+	}
+	if err := stmt.BindText(7, plugin.Summary); err != nil {
+		return err
+	}
+
+	return stmt.Exec()
+}
