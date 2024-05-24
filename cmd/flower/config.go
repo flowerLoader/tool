3e72528c84abe724dd6a8ee928271790c9125e87
Damon Blais <damon@rethunk.tech>
2024-05-23 22:53:17 -0700

    commit LoaderConfig skeleton

---
+++ cmd/flower/config.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+)
+
+type LoaderConfig struct {
+	ID    string
+	Base  []string
+	Build struct {
+		ID          string
+		Entrypoints []string
+	}
+	Map map[string]string
+}
+
+type GameConfig struct {
+	Loader LoaderConfig
+	Meta   struct {
+		Name map[string]string // [en] => "Game Name"
+		OS   []string          `json:"os"`
+	}
+	Subsystem struct {
+		ID      string
+		Appid   int
+		Appname string
+	}
+}
+
+type ProviderConfig struct {
+	Name      string
+	Hosts     []string
+	Schemas   []string
+	Subsystem string
+}
+
+type Config struct {
+	Games     []GameConfig
+	Providers []ProviderConfig
+}
+
+//go:embed main.json
+var MAIN_JSON []byte
+
+func NewConfig() (*Config, error) {
+	toolConfig := new(Config)
+
+	if err := json.Unmarshal(MAIN_JSON, &toolConfig); err != nil {
+		return nil, err
+	}
+
+	return toolConfig, nil
+}
