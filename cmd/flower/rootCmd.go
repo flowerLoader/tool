7177b6bf96f19d7af2896525be66bcb49e955861
Damon Blais <damon@rethunk.tech>
2024-05-23 22:56:09 -0700

    logic for installing flowerLoader core, streamlining

--- cmd/flower/rootCmd.go
+++ cmd/flower/rootCmd.go
@@ -3,43 +3,24 @@ package main
 import (
 	"path/filepath"
 
+	log "github.com/AlbinoGeek/logxi/v1"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
 	"github.com/flowerLoader/tool/pkg/db"
 )
 
-//const PLUGINS_ROOT = "flowerful-plugins"
-////go:embed all:installer
-// var assets embed.FS
-
-var (
-	gameInstallPath  string
-	pluginInputPath  string
-	pluginOutputPath string
-
-	DB *db.DB
-)
+var DB *db.DB
 
 func init() {
-	rootCmd.PersistentFlags().StringVar(&gameInstallPath, "game-path", "",
+	rootCmd.PersistentFlags().String("db-path", "",
+		"Path to the database file (defaults to game's installation directory)")
+	rootCmd.PersistentFlags().String("game-path", "",
 		"Path to the game's installation directory")
-	rootCmd.PersistentFlags().StringVar(&pluginInputPath, "input-path", "dist/src",
+	rootCmd.PersistentFlags().String("input-path", "dist/src",
 		"Path to local plugins to transpile")
-	rootCmd.PersistentFlags().StringVar(&pluginOutputPath, "output-path", "dist/obj",
+	rootCmd.PersistentFlags().String("output-path", "dist/obj",
 		"Path to store transpiled plugins")
 	rootCmd.PersistentFlags().Bool("debug", false, "Scream and shout")
-
-	must := func(err error) {
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	must(viper.BindPFlag("game-path", rootCmd.PersistentFlags().Lookup("game-path")))
-	must(viper.BindPFlag("input-path", rootCmd.PersistentFlags().Lookup("input-path")))
-	must(viper.BindPFlag("output-path", rootCmd.PersistentFlags().Lookup("output-path")))
-	must(viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug")))
 }
 
 var rootCmd = &cobra.Command{
@@ -49,13 +30,27 @@ var rootCmd = &cobra.Command{
 	Long:    "",
 	Args:    cobra.NoArgs,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
+		if val, err := cmd.Flags().GetBool("debug"); val && err == nil {
+			log.InternalLog.SetLevel(log.LevelDebug)
+			log.DefaultLog.SetLevel(log.LevelDebug)
+		}
+
+		gameInstallPath, err := cmd.Flags().GetString("game-path")
+		if err != nil {
+			return err
+		}
+
 		if gameInstallPath, err = resolveGamePath(gameInstallPath); err != nil {
 			return err
 		}
 
-		viper.Set("game-path", gameInstallPath)
-
-		dbPath := filepath.Join(gameInstallPath, "flower.db")
+		dbPath, err := cmd.Flags().GetString("db-path")
+		if err != nil {
+			return err
+		}
+		if dbPath == "" {
+			dbPath = filepath.Join(gameInstallPath, "flower.db")
+		}
 		if DB, err = db.NewDB(dbPath); err != nil {
 			return err
 		}
@@ -64,12 +59,13 @@ var rootCmd = &cobra.Command{
 			return err
 		}
 
-		viper.Set("loaded-db-path", dbPath)
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := cmd.Help(); err != nil {
-			panic(err)
+		cmd.Root().PersistentFlags().Set("game-path", gameInstallPath)
+		cmd.Root().PersistentFlags().Set("db-path", dbPath)
+
+		if err := initFlowerLoader(gameInstallPath); err != nil {
+			return err
 		}
+
+		return nil
 	},
 }
