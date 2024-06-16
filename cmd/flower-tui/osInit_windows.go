d7276c17136a6cb5f3592ff1fd2f7840f8227bf1
Damon Blais <damon@rethunk.tech>
2024-06-16 02:40:55 -0700

    refactor(windows): Improve console initialization with font size and close button control

--- cmd/flower-tui/osInit_windows.go
+++ cmd/flower-tui/osInit_windows.go
@@ -4,32 +4,86 @@
 package main
 
 import (
+	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
 func osInit() {
-	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
-	setConsoleMode := kernel32.NewProc("SetConsoleMode")
-	stdout := windows.Handle(windows.Stdout)
+	// if !mousetrap.StartedByExplorer() {
+	// 	return
+	// }
 
-	// Set all of the following:
-	// ENABLE_VIRTUAL_TERMINAL_PROCESSING
-	// DISABLE_NEWLINE_AUTO_RETURN
-	// ENABLE_PROCESSED_OUTPUT
-	// ENABLE_WRAP_AT_EOL_OUTPUT
-	var mode uint32 = 0x0004 | 0x0008 | 0x0001 | 0x0002
-	setConsoleMode.Call(uintptr(stdout), uintptr(unsafe.Pointer(&mode)))
+	setCloseButtonEnabled(false)
+	setConsoleFontSize(32)
+}
 
-	// Hide the cursor
-	cursorInfo := struct {
-		size    uint32
-		visible int32
-	}{
-		size:    1,
-		visible: 0,
+var (
+	kernel32                = windows.NewLazySystemDLL("kernel32.dll")
+	getConsoleWindow        = kernel32.NewProc("GetConsoleWindow")
+	getStdHandle            = kernel32.NewProc("GetStdHandle")
+	setCurrentConsoleFontEx = kernel32.NewProc("SetCurrentConsoleFontEx")
+
+	user32         = windows.NewLazySystemDLL("user32.dll")
+	enableMenuItem = user32.NewProc("EnableMenuItem")
+	getSystemMenu  = user32.NewProc("GetSystemMenu")
+)
+
+func setCloseButtonEnabled(enabled bool) {
+	const (
+		SC_CLOSE    = 0xF060
+		MF_ENABLED  = 0x00000000
+		MF_GRAYED   = 0x00000001
+		MF_DISABLED = 0x00000002
+	)
+
+	// Get our hWnd, then get the system menu for the console window.
+	hWnd, _, _ := getConsoleWindow.Call()
+	hMenu, _, _ := getSystemMenu.Call(hWnd, 0)
+
+	// Disable the close button.
+	if enabled {
+		enableMenuItem.Call(hMenu, SC_CLOSE, MF_ENABLED)
+	} else {
+		enableMenuItem.Call(hMenu, SC_CLOSE, MF_GRAYED)
 	}
-	setConsoleCursorInfo := kernel32.NewProc("SetConsoleCursorInfo")
-	setConsoleCursorInfo.Call(uintptr(stdout), uintptr(unsafe.Pointer(&cursorInfo)))
+}
+
+func setConsoleFontSize(fontSize int) {
+	stdOutputHandle, _, _ := getStdHandle.Call(windows.STD_OUTPUT_HANDLE)
+	type CONSOLE_FONT_INFOEX struct {
+		cbSize     uint32
+		nFont      uint32
+		dwFontSize windows.Coord
+		FontFamily uint32
+		FontWeight uint32
+		FaceName   [32]uint16
+	}
+	setConsoleOutputCP := kernel32.NewProc("SetConsoleOutputCP")
+	var cp uint32 = 437
+	setConsoleOutputCP.Call(uintptr(cp))
+	setCurrentConsoleFontEx.Call(
+		stdOutputHandle,
+		0,
+		uintptr(unsafe.Pointer(&CONSOLE_FONT_INFOEX{
+			cbSize:     uint32(unsafe.Sizeof(CONSOLE_FONT_INFOEX{})),
+			nFont:      0,
+			dwFontSize: windows.Coord{X: 0, Y: int16(fontSize)},
+			FontFamily: 54,  // FF_MODERN << 4 | TMPF_VECTOR | TMPF_TRUETYPE
+			FontWeight: 400, // 400 = normal, 700 = bold
+			FaceName:   stringToUint16Array("Consolas"),
+		})),
+	)
+}
+
+func stringToUint16Array(s string) [32]uint16 {
+	utf16, err := syscall.UTF16FromString(s)
+	if err != nil {
+		panic(err)
+	}
+
+	var arr [32]uint16
+	copy(arr[:], utf16)
+	return arr
 }
